893da58b48e3e25698b4c1796855a037bcdfffa9
Thomas_JACOTIN <thomas.jacotin@ecole-isitech.fr>
2025-11-17 15:28:02 +0100

    PremierDepot

---
+++ system_info.py
@@ -0,0 +1,35 @@
+##Import Librairies
+import socket
+import platform
+
+##Variables
+Nom_PC = socket.gethostname()   ## Information Nom PC
+Adresse_IP = socket.gethostbyname(Nom_PC)   ## Information Adresse IP
+Architecture_processeur = platform.machine()    ## information Architecture processeur
+Systeme = platform.system(), platform.version()   ## Information Windows + build
+Processeur = platform.processor()   ## Information Processeur 
+Nom_Domaine = socket.getfqdn([Nom_PC])  ## Information nom domaine
+
+Info_Python = platform.python_build()   ## Information Version Python
+Info_Python_Compiler = platform.python_compiler()   ## Information Compilateur Python
+
+
+##Affichage:
+print("")
+print("=====================")
+print("Information Système")
+print("=====================")
+print("")
+print("Nom du PC: ", Nom_PC, "--", Adresse_IP)
+print("processeur", Processeur, Architecture_processeur)
+print("Système d'exploitation: ", Systeme)
+print("Nom de domaine : ", Nom_Domaine)
+print("")
+print("=====================")
+print("Information Python")
+print("=====================")
+print("")
+print("information python :", Info_Python)
+print("Compilleur python : ", Info_Python_Compiler)
+
+
